8ad757009753c1baefc8a763afffc281a35c28ea
MaurZac <maur.mx@gmail.com>
2025-01-23 15:56:45 -0600

    Initial Commit

---
+++ PetcoTeams/PetcoTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetcoTeamsApp.swift
+//  PetcoTeams
+//
+//  Created by MaurZac on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PetcoTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetcoTeamsTests/PetcoTeamsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetcoTeamsTests.swift
+//  PetcoTeamsTests
+//
+//  Created by MaurZac on 23/01/25.
+//
+
+import Testing
+@testable import PetcoTeams
+
+struct PetcoTeamsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
